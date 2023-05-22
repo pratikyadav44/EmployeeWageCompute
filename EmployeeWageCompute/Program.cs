f942d594a14ede3cfa1a3ac7d75e211e8f881e85
pratikyadav44 <pratik.yadav2@gmail.com>
2023-05-22 17:44:44 +0530

    Employee_Attendance

--- EmployeeWageCompute/Program.cs
+++ EmployeeWageCompute/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
+            const int Is_Present = 1;
             Console.WriteLine("Welcome to Employee Wage Computation");
+            Random random = new Random();
+            int randomInput = random.Next(0, 2);
+            if (Is_Present == randomInput)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }
