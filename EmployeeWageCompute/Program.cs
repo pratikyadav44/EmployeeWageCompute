1f74b2f3499f662eb99f797aa312d3988130c053
pratikyadav44 <pratik.yadav2@gmail.com>
2023-05-22 18:10:13 +0530

    Employee_DailyHrs

--- EmployeeWageCompute/Program.cs
+++ EmployeeWageCompute/Program.cs
@@ -4,20 +4,27 @@
     {
         static void Main(string[] args)
         {
-            const int Is_Present = 1;
+            const int Full_Time = 1;
+            const int Part_Time = 2;
             const int Emp_Rate_Per_Hr = 20;
             int EmpHrs = 0;
             int EmpWage = 0;
             Console.WriteLine("Welcome to Employee Wage Computation");
             Random random = new Random();
             int randomInput = random.Next(0, 2);
-            if (Is_Present == randomInput)
+            if (Full_Time == randomInput)
             {
+                EmpHrs = 8;
                 Console.WriteLine("Employee is Present");
+            }
+            else if (Part_Time == randomInput)
+            {
+                EmpHrs = 4;
+                Console.WriteLine("Employee is Present Part-time");
             }
             else
             {
-                Console.WriteLine("Employee is absent");
+                Console.WriteLine("Employee is Absent");
             }
             EmpWage = Emp_Rate_Per_Hr * EmpHrs;
             Console.WriteLine("Employee Wage Per Day:{0}", EmpWage);
