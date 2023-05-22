5562ddc8a2d9758f63b7cc094992119d55682868
pratikyadav44 <pratik.yadav2@gmail.com>
2023-05-22 17:59:21 +0530

    Employee_Daily_Wage

--- EmployeeWageCompute/Program.cs
+++ EmployeeWageCompute/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             const int Is_Present = 1;
+            const int Emp_Rate_Per_Hr = 20;
+            int EmpHrs = 0;
+            int EmpWage = 0;
             Console.WriteLine("Welcome to Employee Wage Computation");
             Random random = new Random();
             int randomInput = random.Next(0, 2);
@@ -16,6 +19,9 @@
             {
                 Console.WriteLine("Employee is absent");
             }
+            EmpWage = Emp_Rate_Per_Hr * EmpHrs;
+            Console.WriteLine("Employee Wage Per Day:{0}", EmpWage);
+
         }
     }
 }
