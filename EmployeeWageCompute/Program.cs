38aa7500ffa065db69fe24186ea64b666077b866
pratikyadav44 <pratik.yadav2@gmail.com>
2023-05-22 18:54:56 +0530

    ConditionalWage

--- EmployeeWageCompute/Program.cs
+++ EmployeeWageCompute/Program.cs
@@ -8,34 +8,39 @@
             const int Part_Time = 2;
             const int Emp_Rate_Per_Hr = 20;
             const int Max_Working_Days = 20;
+            const int Max_Working_Hrs = 100;
             int EmpHrs = 0;
             int EmpWage = 0;
             int TotalWage = 0;
+            int TotalHrs = 0;
+            int day = 1;
             Console.WriteLine("Welcome to Employee Wage Computation");
             Random random = new Random();
-            for (int day = 1; day <= Max_Working_Days; day++)
+            while (day <= Max_Working_Days && TotalHrs < Max_Working_Hrs )
             {
                 int randomInput = random.Next(0, 3);
                 switch (randomInput)
                 {
                     case Full_Time:
                         EmpHrs = 8;
-                        Console.WriteLine("Employee is Present");
+                       // Console.WriteLine("Employee is Present");
                         break;
 
                     case Part_Time:
                         EmpHrs = 4;
-                        Console.WriteLine("Employee is Present Part-time");
+                        //Console.WriteLine("Employee is Present Part-time");
                         break;
-                    default:
+                    /*default:
                         Console.WriteLine("Employee is Absent");
-                        break;
+                        break;*/
                 }
                 EmpWage = Emp_Rate_Per_Hr * EmpHrs;
                 TotalWage = EmpWage + TotalWage;
+                TotalHrs = EmpHrs + TotalHrs;
                 Console.WriteLine("Employee Wage Per Day{0}:{1}",day, EmpWage);
+                day++;
             }
-            Console.WriteLine("Total Wage For {0} days is {1}", Max_Working_Days, TotalWage);
+            Console.WriteLine("Total Wage For {0} days and {1} Hrs is: {2}",(day - 1),TotalHrs, TotalWage);
         }   
     }
 }
